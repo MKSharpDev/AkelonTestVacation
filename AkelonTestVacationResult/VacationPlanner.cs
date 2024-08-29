c7d753c5281f7d9745e613c130142b5d9aa9f96c
M K <kuzz.ma@yandex.ru>
2024-08-29 15:25:58 +0300

    fix

--- AkelonTestVacationResult/VacationPlanner.cs
+++ AkelonTestVacationResult/VacationPlanner.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Xml.Linq;
+
 public class VacationPlanner
 {
     private readonly ICollection<string> _workerList;
@@ -20,6 +22,7 @@ public class VacationPlanner
 
         Dictionary<string, List<Vacation>> vacationDictionary = new Dictionary<string, List<Vacation>>();
 
+        List<Vacation> Vacations = new List<Vacation>();
         DateTime yearStart = new DateTime(_year, 1, 1);
         DateTime yearEnd = new DateTime(_year, 12, 31);
         string[] vacationSteps = { "7", "14" };
@@ -68,28 +71,49 @@ public class VacationPlanner
                     bool existStart = false;
                     bool existEnd = false;
 
-                    //проверяем нет ли заранее запланированного отпуска в планируемое время 
-                    if (!plan.Value.Any(element => element.StartDate >= vacationStartDate && element.EndDate <= vacationEndDate))
+                    //проверяем нет ли заранее запланированного отпуска в планируемое время кого то в отделе:
+                    // не начинается ли отпуск между планируемым концом и началом отпуска || не оканчивается
+                    // ли отпуск между планируемым концом и началом отпуска || не входит ли отпук полностью в чей то
+                    if (!Vacations.Any(element => (element.StartDate >= vacationStartDate && element.StartDate <= vacationEndDate) ||
+                        (element.EndDate >= vacationStartDate && element.EndDate <= vacationEndDate) ||
+                        (element.StartDate <= vacationStartDate && vacationEndDate <= element.EndDate)))
                     {
-                        //Принимаем за условие, что отпуск не может быть раньше чем через месяц от прошлого отпуска
+                        //проверяем нет ли заранее запланированного отпуска в пределах 3х дней в планируемое время
+                        if (!Vacations.Any(element => (element.StartDate.AddDays(3) >= vacationStartDate && element.StartDate.AddDays(3) <= vacationEndDate) || 
+                            (element.EndDate.AddDays(-3) >= vacationStartDate && element.EndDate.AddDays(-3) <= vacationEndDate)))
+                        {
+                            //Принимаем за условие, что отпуск не может быть раньше чем через месяц от прошлого отпуска
 
-                        //проверяем нет ли начала отпуска позже в течении месяца от планируемого конца отпуска
-                        existStart = plan.Value.Any(element => element.StartDate >= vacationEndDate && element.StartDate <= vacationEndDate.AddMonths(1));
+                            //проверяем нет ли начала отпуска позже в течении месяца от планируемого конца отпуска
+                            existStart = plan.Value.Any(element => element.StartDate >= vacationEndDate && element.StartDate <= vacationEndDate.AddMonths(1));
 
-                        //проверяем нет ли конца отпуска ранее в течении месяца от планируемого начала отпуска
-                        existEnd = plan.Value.Any(element => element.EndDate <= vacationStartDate && element.EndDate >= vacationStartDate.AddMonths(-1));
-                        if (!existStart && !existEnd)
-                            canCreateVacation = true;
+                            //проверяем нет ли конца отпуска ранее в течении месяца от планируемого начала отпуска
+                            existEnd = plan.Value.Any(element => element.EndDate <= vacationStartDate && element.EndDate >= vacationStartDate.AddMonths(-1));
+                            if (!existStart && !existEnd)
+                            {
+                                canCreateVacation = true;
+                            }
+                        }
                     }
 
                     if (canCreateVacation)
                     {
+                        var Id = Guid.NewGuid();
                         //добавляем отпуск данному работнику
-                        plan.Value.Add(new Vacation() { 
-                            Id = Guid.NewGuid(),
-                            StartDate = vacationStartDate, 
-                            EndDate = vacationEndDate });
-                       
+                        plan.Value.Add(new Vacation()
+                        {
+                            Id = Id,
+                            StartDate = vacationStartDate,
+                            EndDate = vacationEndDate
+                        });
+
+                        Vacations.Add(new Vacation()
+                        {
+                            Id = Id,
+                            StartDate = vacationStartDate,
+                            EndDate = vacationEndDate
+                        });
+
                         vacationCount = vacationCount - difference;
                     }
                 }
